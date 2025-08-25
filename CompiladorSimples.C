1739e2d58ad4fd3e5cffd41610991554742e8c0b
Mariana Lima <marianadsantoslima@gmail.com>
2025-08-25 20:07:18 -0300

    Add files via upload

---
+++ CompiladorSimples.C
@@ -0,0 +1,20 @@
+# Meu Compilador
+
+Pojeto de um compilador simples dentro do Git desenvolvido em C para estudo.
+
+## 📌 Notas
+- Funciona em Linux e Windows
+- Requer GCC ou MinGW
+- Foi testado no Ubuntu 24.04
+
+## 🚀 Abrir code space no Compilador 
+
+1. criar **repositorio**
+2. criar **arquivo**
+3. clicar em **repositorio** e abrir o **codigo** 
+4. criar um novo espaço de codigo → **codespaces**
+5. No codespace criar **code space Main** e escrever seu código
+6.  Configurar o compilador no terminal do Codespace:
+   ```bash
+   gcc main.c -o compilador
+   ./compilador
